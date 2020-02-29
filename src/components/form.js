d67f02475bd3a566f60de474ec9d5030285093ac
josh-gehmlich <joshuagehmlich29@gmail.com>
2020-02-29 11:29:49 -0500

    Embed TypeForm

---
+++ src/components/form.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function () {
+  require('../typeform');
+  return (
+    <div
+      className="typeform-widget"
+      data-url="https://elliotbonneville1.typeform.com/to/H0zlqT"
+      data-transparency="50"
+      data-hide-headers={"true"}
+      data-hide-footer={"true"}
+      style={{
+        width: "100%",
+        height: "500px"
+      }}
+    />
+  )
+}
